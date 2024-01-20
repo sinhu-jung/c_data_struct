cc575170fc9d05ae4a084285088118bc7b332134
sinhu <tlsgn26@naver.com>
2024-01-20 12:57:08 +0900

    초기 커밋.

---
+++ c_data_struct/BasicLinkedList.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+typedef struct USERDATA
+{
+	int age;
+	char name[32];
+	char phone[32];
+	struct USERDATA* pNext;
+} USERDATA;
+
+int main(void) {
+	USERDATA aList[4] = {
+		{20, "Tester01", "010-1234-5678", NULL},
+		{21, "Tester02", "010-1234-5678", NULL},
+		{22, "Tester03", "010-1234-5678", NULL},
+		{23, "Tester04", "010-1234-5678", NULL},
+	};
+
+	aList[0].pNext = &aList[1];
+	aList[1].pNext = &aList[2];
+	aList[2].pNext = &aList[3];
+	aList[3].pNext = NULL;
+
+	return 0;
+}
